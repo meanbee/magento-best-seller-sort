5b3b274ed0eee69c9e06af6f7792a3645067b5a1
Nick Jones <nick@nicksays.co.uk>
2014-01-31 15:06:44 +0000

    Downcase the module name as the class alias

--- app/code/community/Meanbee/AdvancedSort/Helper/Data.php
+++ app/code/community/Meanbee/AdvancedSort/Helper/Data.php
@@ -3,7 +3,7 @@
 class Meanbee_AdvancedSort_Helper_Data extends Mage_Core_Helper_Abstract
 {
     const ATTRIBUTE_NAME_QTY_ORDERED = 'qty_ordered';
-    const XML_PATH_QTY_ORDERED_AGE = 'Meanbee_AdvancedSort/general/qty_ordered_age';
+    const XML_PATH_QTY_ORDERED_AGE = 'meanbee_advancedsort/general/qty_ordered_age';
 
     /**
      * Get age of orders that should be looked for when updating qty ordered values.
