9fee310f4440018cebb6a3a9788570a5590b9719
Ash Smith <ash.smith@meanbee.com>
2014-02-20 13:12:36 +0000

    README added, and changed version

---
+++ README.md
@@ -0,0 +1,19 @@
+#Meanbee_BestSellerSort
+
+Meanbee BestSellerSort adds the ability for you to sort your products by Best Sellers over a period of time, default is 30 days, allow you to keep on promoting the best products to your customers and making them much easier to find in your listings.
+
+###Features:
+
+- Add new sort option to category pages, allowing customers to see the best selling products.
+- Best sellers is generated automatically, and recalculated each day to keep the data relevant. No manual work needed!
+- Putting your best selling products towards the top of the category will help your customers find the best products, the ones they are most likely to buy! The result? Increased conversions and sales.
+
+###How to customise:
+
+The date range this module uses can be configured by you, allowing you to define the exact number previous days sales that should be used when calculating the best seller positioning. 
+
+Under `System > Configuration > Catalog > Meanbee: Best Seller Sort` you will see two configuration options. The first allows you to re-calculate the best sellers manually (this happens automatically each night, but you may wish to recalculate sooner). The second allows you define the number of days used in the best seller calculation.
+
+###Release History
+
+- V1.0.0 - Initial Release
--- app/code/community/Meanbee/BestSellerSort/sql/meanbee_bestsellersort_setup/mysql4-install-1.0.0.php
+++ app/code/community/Meanbee/BestSellerSort/sql/meanbee_bestsellersort_setup/mysql4-install-1.0.0.php
@@ -5,21 +5,22 @@ $installer = $this;
 $installer->startSetup();
 
 $installer->addAttribute('catalog_product', Meanbee_BestSellerSort_Helper_Data::ATTRIBUTE_NAME_QTY_ORDERED, array(
-    'type'              => 'int',
-    'backend'           => '',
-    'frontend_class'    => 'validate-digits',
-    'label'             => 'Best Sellers',
-    'input'             => 'text',
-    'class'             => '',
-    'source'            => '',
-    'global'            => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_GLOBAL,
-    'visible'           => false,
-    'required'          => false,
-    'apply_to'          => '',
-    'is_configurable'   => false,
-    'note'              => '',
+    'type'                    => 'int',
+    'backend'                 => '',
+    'frontend_class'          => 'validate-digits',
+    'label'                   => 'Best Sellers',
+    'input'                   => 'text',
+    'class'                   => '',
+    'source'                  => '',
+    'global'                  => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_GLOBAL,
+    'visible'                 => false,
+    'required'                => false,
+    'apply_to'                => '',
+    'is_configurable'         => false,
+    'note'                    => '',
     'used_in_product_listing' => true,
-    'used_for_sort_by'        => true
+    'used_for_sort_by'        => true,
+    'default_value'           => 0
 ));
 
 // Schedule the task to run right away.
